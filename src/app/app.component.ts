460aeafa08a306d13073d383cc4c67f3bbe2a732
Kaleem <kaleem14you@gmail.com>
2024-08-12 02:45:35 +0500

    material UI demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatButtonModule} from '@angular/material/button';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import {FormsModule} from '@angular/forms';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,MatButtonModule, MatDividerModule, MatIconModule,MatButtonToggleModule,FormsModule,
+    MatCheckboxModule,MatProgressBarModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
